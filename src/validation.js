b566eb045d1b7859739264b37756b0fbab12bde1
ksib7 <sibirkin70@gmail.com>
2022-02-22 00:08:43 +0300

    Changed some validation items

--- src/validation.js
+++ src/validation.js
@@ -10,15 +10,15 @@ let form = document.querySelector('.form--js');
 // for footer
 formEl.addEventListener('submit', function (event) {
     if (emailEl.value == '' || textEl.value == '') {
-        alert('Заполните поле ввода.');
         event.preventDefault();
+        alert('Заполните поле ввода.');
     }
 });
 
 // for registration form
 form.addEventListener('submit', function (event) {
     if (email.value == '' || textEl.value == '') {
-        alert('Заполните поле ввода.');
         event.preventDefault();
+        alert('Заполните поле ввода.');
     }
 });
